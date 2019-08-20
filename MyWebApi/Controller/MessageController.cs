590f04d8fe74893049414eda32a1e53a19eff7c9
tavisca-ysant <ysant@tavisca.in>
2019-08-20 14:40:27 +0530

    Added Readme file

---
+++ MyWebApi/Controller/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MyWebApi.Controller
+{
+    public class MessageController : ApiController
+    {
+        public string Get(string token)
+        {
+            if (token == "hello")
+                return "hi";
+            else if (token == "hi")
+                return "hello";
+            return "Invalid Input";
+        }
+    }
+}
